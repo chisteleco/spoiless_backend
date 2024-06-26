fb0254da41704b3d385a5039346649675760f280
chisteleco <35273095+chisteleco@users.noreply.github.com>
2024-06-26 17:04:08 +0000

    first version

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response,NextFunction } from 'express'; 
+import dotenv from 'dotenv'; // Add this 
+dotenv.config(); // And this 
+
+const app = express(); 
+
+const port = process.env.PORT || 3000; 
+
+app.get('/', (req: Request, res: Response) => { 
+  res.send('Hello there!'); 
+}); 
+
+app.use((err: Error, req: Request, res: Response, _next : NextFunction) => { 
+  console.error(err.stack); 
+  res.status(500).send('Something went wrong'); 
+}); 
+
+app.listen(port, () => { 
+  console.log('Server running at http://localhost:${port}'); 
+});
